26f13fffeaa9557538c7d3034ae08a96dd80bc01
Nishudana Gadapa <nishudana.g@gmail.com>
2024-12-03 07:29:55 +0530

    import changed in login

--- Client/Food-Delivery-App/src/Pages/Auth/LoginPage/login.jsx
+++ Client/Food-Delivery-App/src/Pages/Auth/LoginPage/login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./Login.module.css";
+import styles from "./login.module.css";
 import { login } from "../../../Services/auth.service";
 import Footer from "../../Common/Footer/footer";
 import mainImage from "../../../assets/MainPageImage.png";
